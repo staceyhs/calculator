9dd0e1ea82538860ab2089c47de6d029b1c49a25
Stacey Strovink <staceystrovink@gmail.com>
2020-02-17 12:29:02 -0800

    add addition JS coding

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
-var add=function(number1, number2) {
-  return number1 + number2;
-}
+// Business logic:
 
-var number1=parseInt(prompt("Enter a number:"));
-var number2=parseInt(prompt("Enter another number:"));
-var result=add(number1, number2);
-alert(result);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// Everything below this line is user interface logic:
+
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2); 
+    $("#output").text(result);
+  });
+});
